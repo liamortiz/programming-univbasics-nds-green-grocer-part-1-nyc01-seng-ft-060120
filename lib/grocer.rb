806f8604f1719f828a8852d7ff6c10a08b95ade9
Leamsi Escribano <leamsii.e@gmail.com>
2020-05-13 23:08:50 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -11,17 +11,12 @@ def consolidate_cart(cart)
   # REMEMBER: This returns a new Array that represents the cart. Don't merely
   # change `cart` (i.e. mutate) it. It's easier to return a new thing.
   
-  # For each repetition of an item, it needs to update the other array with the count value
-  
-  
-  # Need to check if an object is already in an array
-  # If is not then add it
-  # Do it based on their names
   filtered_array = []
+  
   cart.each do |item|
-    tmp = find_item_by_name_in_collection(item[:item], filtered_array)
-    if tmp
-      tmp[:count] += 1
+    target_item = find_item_by_name_in_collection(item[:item], filtered_array)
+    if target_item
+      target_item[:count] += 1
     else
       item[:count] = 1
       filtered_array << item
