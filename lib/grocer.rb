ac7c5b1454046b3f98b82f6a1b561fe57d590d4e
Leamsi Escribano <leamsii.e@gmail.com>
2020-05-13 22:32:15 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -1,10 +1,10 @@
 def find_item_by_name_in_collection(name, collection)
   collection.each do |item|
     if item[:item] == name
-      item
+      return item
     end
   end
-
+  nil
 end
 
 def consolidate_cart(cart)
